da6e6f8bef020ba93dbdbef025163b1e11f7b8ca
kobayashi_kazuto <kazunoko0907@gmail.com>
2020-12-30 18:18:19 +0900

    update:受信データ側もペアをつくるようにして、updateの関数の処理を書いた

--- mcp3208t_reader.cpp
+++ mcp3208t_reader.cpp
@@ -1,6 +1,6 @@
 #include "mcp3208t_reader.hpp"
 
-Mcp3208t_reader::Mcp3208t_reader(GPIO_TypeDef &nss_port, uint16_t &nss_pin):nss_port(&nss_port),nss_pin(nss_pin){
+Mcp3208t_reader::Mcp3208t_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef &nss_port, uint16_t &nss_pin):hspi(&hspi),nss_port(&nss_port),nss_pin(nss_pin){
 
 }
 
@@ -8,10 +8,12 @@ void Mcp3208t_reader::init(){
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_SET);
 	for(uint8_t i = 0; i < 8; i++){
 		channel_config.insert(std::make_pair(static_cast<ADC_CHANNEL>(i), (i+8)<<12));
+		receive_port.insert(std::make_pair(static_cast<ADC_CHANNEL>(i),receive_data[i]));
 	}
 }
 
-void Mcp3208t_reader::update(){
+void Mcp3208t_reader::update(ADC_CHANNEL adc_channel, uint32_t timeout){
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_RESET);
-
+	HAL_SPI_TransmitReceive(hspi,(uint8_t*)&(channel_config.at(adc_channel)), (uint8_t*)&(receive_port.at(adc_channel)), sizeof(receive_port.at(adc_channel)), timeout);
+	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_SET);
 }
--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -19,17 +19,16 @@
 class Mcp3208t_reader{
 private:
 	std::map<ADC_CHANNEL, uint16_t> channel_config;
-
+	std::map<ADC_CHANNEL, uint16_t> receive_port;
 	std::array<uint16_t, 8> receive_data;
 	std::array<uint16_t, 8> transmit_data;
 	GPIO_TypeDef* nss_port;
 	const uint16_t nss_pin;
+	SPI_HandleTypeDef* hspi;
 public:
-	Mcp3208t_reader(GPIO_TypeDef &nss_port, uint16_t &nss_pin);
+	Mcp3208t_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef &nss_port, uint16_t &nss_pin);
 	void init();
-	void update();
-	void get();
-	void update_all();
-	void get_all();
+	void update(ADC_CHANNEL adc_channel, uint32_t timeout);
+	void get(ADC_CHANNEL adc_channel);
 };
 #endif //INC_MCP3208T_READER_HPP_
