c6d46a95a9e09d657ace74de2127aed962c98557
kobayashi_kazuto <kazunoko0907@gmail.com>
2020-12-30 17:01:19 +0900

    update:gpioとspiのヘッダをインクルードし、コンストラクタ」の引数を書いた

--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -1,15 +1,16 @@
 #ifndef INC_MCP3208T_READER_HPP_
 #define INC_MCP3208T_READER_HPP_
 #include <array>
-
+#include "gpio.h"
+#include "spi.h"
 class Mcp3208t_reader{
 private:
 	std::array<uint16_t, 8> receive_data;
 	std::array<uint16_t, 8> transmit_data;
-	GPIO_TypeDef* nss_port;
-	uint16_t nss_pin;
+	const GPIO_TypeDef* nss_port;
+	const uint16_t nss_pin;
 public:
-	Mcp3208t_reader();
+	Mcp3208t_reader(GPIO_TypeDef &nss_port, uint16_t &nss_pin);
 	void init();
 	void update();
 	void get();
