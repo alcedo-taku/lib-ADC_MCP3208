7968123c9872569d67683fecfda416fbe5631699
kobayashi_kazuto <kazunoko0907@gmail.com>
2020-12-30 17:27:32 +0900

    initでadcを行うチャンネルのペアをつくるようにした

--- mcp3208t_reader.cpp
+++ mcp3208t_reader.cpp
@@ -6,8 +6,12 @@ Mcp3208t_reader::Mcp3208t_reader(GPIO_TypeDef &nss_port, uint16_t &nss_pin):nss_
 
 void Mcp3208t_reader::init(){
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_SET);
+	for(uint8_t i = 0; i < 8; i++){
+		channel_config.insert(std::make_pair(static_cast<ADC_CHANNEL>(i), (i+8)<<12));
+	}
 }
 
 void Mcp3208t_reader::update(){
+	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_RESET);
 
 }
--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -3,8 +3,23 @@
 #include <array>
 #include "gpio.h"
 #include "spi.h"
+#include <map>
+
+	enum class ADC_CHANNEL{
+		CH_0,
+		CH_1,
+		CH_2,
+		CH_3,
+		CH_4,
+		CH_5,
+		CH_6,
+		CH_7
+	};
+
 class Mcp3208t_reader{
 private:
+	std::map<ADC_CHANNEL, uint16_t> channel_config;
+
 	std::array<uint16_t, 8> receive_data;
 	std::array<uint16_t, 8> transmit_data;
 	GPIO_TypeDef* nss_port;
