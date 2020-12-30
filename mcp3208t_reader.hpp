326e0dbf6c3909e1e4916967f09c242f9c9f8b79
kobayashi_kazuto <kazunoko0907@gmail.com>
2020-12-30 16:49:53 +0900

    update: classの実装部分を書いた

--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -1,4 +1,19 @@
 #ifndef INC_MCP3208T_READER_HPP_
 #define INC_MCP3208T_READER_HPP_
+#include <array>
 
+class Mcp3208t_reader{
+private:
+	std::array<uint16_t, 8> receive_data;
+	std::array<uint16_t, 8> transmit_data;
+	GPIO_TypeDef* nss_port;
+	uint16_t nss_pin;
+public:
+	Mcp3208t_reader();
+	void init();
+	void update();
+	void get();
+	void update_all();
+	void get_all();
+};
 #endif //INC_MCP3208T_READER_HPP_
