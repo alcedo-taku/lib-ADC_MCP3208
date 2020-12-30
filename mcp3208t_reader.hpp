1d10e26f7a28c0a593c62d7a07610bf8e27332a6
kobayashi_kazuto <kazunoko0907@gmail.com>
2020-12-30 17:08:08 +0900

    update:nss_portのconstを外した

--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -7,7 +7,7 @@ class Mcp3208t_reader{
 private:
 	std::array<uint16_t, 8> receive_data;
 	std::array<uint16_t, 8> transmit_data;
-	const GPIO_TypeDef* nss_port;
+	GPIO_TypeDef* nss_port;
 	const uint16_t nss_pin;
 public:
 	Mcp3208t_reader(GPIO_TypeDef &nss_port, uint16_t &nss_pin);
