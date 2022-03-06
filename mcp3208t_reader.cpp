170bbd4d9badd6967cfed2c7c35964a17d883459
立脇 拓 <takuji.coroton.thanks@gmail.com>
2022-03-06 16:06:21 +0900

    style: 改行やスペースを調整

--- mcp3208t_reader.cpp
+++ mcp3208t_reader.cpp
@@ -6,8 +6,15 @@
  * @param nss_port SSピンのPort 
  * @param nss_pin SSピンのPin
  */
-Mcp3208t_reader::Mcp3208t_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef *nss_port, uint16_t nss_pin):hspi(&hspi),nss_port(nss_port),nss_pin(nss_pin){
-
+Mcp3208t_reader::Mcp3208t_reader(
+	SPI_HandleTypeDef& hspi,
+	GPIO_TypeDef *nss_port,
+	uint16_t nss_pin
+):
+	hspi(&hspi),
+	nss_port(nss_port),
+	nss_pin(nss_pin)
+{
 }
 
 /**
@@ -20,7 +27,7 @@ void Mcp3208t_reader::init(){
 		transmit_data[i][1] = i << 6;
 		transmit_data[i][2] = 0;
 		channel_config.insert(std::make_pair(static_cast<ADC_CHANNEL>(i), transmit_data[i]));
-			receive_port.insert(std::make_pair(static_cast<ADC_CHANNEL>(i),receive_data[i]));
+		receive_port.insert(std::make_pair(static_cast<ADC_CHANNEL>(i),receive_data[i]));
 	}
 }
 
@@ -41,5 +48,5 @@ void Mcp3208t_reader::update(ADC_CHANNEL adc_channel, uint32_t timeout){
  * @return uint16_t 指定したのChannelの値
  */
 uint16_t Mcp3208t_reader::get(ADC_CHANNEL adc_channel){
-	return static_cast<uint16_t>((receive_port.at(adc_channel)[1]<<8 | receive_port.at(adc_channel)[2]))<<3;
+	return static_cast<uint16_t>((receive_port.at(adc_channel)[1]<<8 | receive_port.at(adc_channel)[2])) <<3;
 }
