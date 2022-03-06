e4417da71a392491dc421fc4bc056e1584e49d76
立脇 拓 <takuji.coroton.thanks@gmail.com>
2022-03-06 15:56:25 +0900

    docs: Doxygen 形式でコメントを書いた

--- mcp3208t_reader.cpp
+++ mcp3208t_reader.cpp
@@ -1,12 +1,20 @@
 #include "mcp3208t_reader.hpp"
 
+/**
+ * コンストラクタ
+ * @param hspi SPI番号
+ * @param nss_port SSピンのPort 
+ * @param nss_pin SSピンのPin
+ */
 Mcp3208t_reader::Mcp3208t_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef *nss_port, uint16_t nss_pin):hspi(&hspi),nss_port(nss_port),nss_pin(nss_pin){
 
 }
 
+/**
+ * 初期化関数
+ */
 void Mcp3208t_reader::init(){
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_SET);
-
 	for(uint8_t i = 0; i < 8; i++){
 		transmit_data[i][0] = 0b110 | i>>2;
 		transmit_data[i][1] = i << 6;
@@ -16,13 +24,22 @@ void Mcp3208t_reader::init(){
 	}
 }
 
+/**
+ * ADCICと通信し、指定したChannelの値を更新する関数
+ * @param adc_channel 更新したいChannel
+ * @param timeout SPI通信のtimeout
+ */
 void Mcp3208t_reader::update(ADC_CHANNEL adc_channel, uint32_t timeout){
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_RESET);
 	HAL_SPI_TransmitReceive(hspi,(uint8_t*)&(channel_config[adc_channel]), (uint8_t*)&(receive_port[adc_channel]), sizeof(receive_port[adc_channel]), timeout);
 	HAL_GPIO_WritePin(nss_port, nss_pin, GPIO_PIN_SET);
 }
 
+/**
+ * 指定したのChannelの値を取得する関数
+ * @param adc_channel 取得したいChannel
+ * @return uint16_t 指定したのChannelの値
+ */
 uint16_t Mcp3208t_reader::get(ADC_CHANNEL adc_channel){
-
 	return static_cast<uint16_t>((receive_port.at(adc_channel)[1]<<8 | receive_port.at(adc_channel)[2]))<<3;
 }
--- mcp3208t_reader.hpp
+++ mcp3208t_reader.hpp
@@ -5,30 +5,37 @@
 #include "spi.h"
 #include <unordered_map>
 
-	enum class ADC_CHANNEL:uint8_t{
-		CH_0,
-		CH_1,
-		CH_2,
-		CH_3,
-		CH_4,
-		CH_5,
-		CH_6,
-		CH_7
-	};
+/**
+ * 各Channelを要素に持つenum class
+ */
+enum class ADC_CHANNEL:uint8_t{
+	CH_0,
+	CH_1,
+	CH_2,
+	CH_3,
+	CH_4,
+	CH_5,
+	CH_6,
+	CH_7
+};
 
+/**
+ * ADコンバータ MCP3208 から値を読み取るクラス
+ */
 class Mcp3208t_reader{
 private:
-	std::unordered_map<ADC_CHANNEL, std::array<uint8_t,3>> channel_config;
-	std::unordered_map<ADC_CHANNEL, std::array<uint8_t,3>> receive_port;
-	std::array<std::array<uint8_t,3>, 8> receive_data;
-	std::array<std::array<uint8_t,3>, 8> transmit_data;
-	SPI_HandleTypeDef* hspi;
-	GPIO_TypeDef* nss_port;
-	const uint16_t nss_pin;
+	std::unordered_map<ADC_CHANNEL, std::array<uint8_t,3>> channel_config;	//!< ICから値を受け取る変数
+	std::unordered_map<ADC_CHANNEL, std::array<uint8_t,3>> receive_port;   	//!< ICに送る変数(値を取得したいChannelが格納)
+	std::array<std::array<uint8_t,3>, 8> receive_data;						//!< ICから値を受け取る変数
+	std::array<std::array<uint8_t,3>, 8> transmit_data;						//!< ICに送る変数(値を取得したいChannelが格納)
+	SPI_HandleTypeDef* hspi;												//!< SPI番号
+	GPIO_TypeDef* nss_port;													//!< SSピンのPort
+	const uint16_t nss_pin;													//!< SSピンのPin
 public:
 	Mcp3208t_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef *nss_port, uint16_t nss_pin);
 	void init();
 	void update(ADC_CHANNEL adc_channel, uint32_t timeout);
 	uint16_t get(ADC_CHANNEL adc_channel);
 };
+
 #endif //INC_MCP3208T_READER_HPP_
