66397992a5aa3cff5fb56e798b44a0fdb8f3f394
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-12 14:49:48 +0900

    refactor: namespace mcp3208 を追加

--- mcp3208.hpp
+++ mcp3208.hpp
@@ -4,11 +4,13 @@
 #include "gpio.h"
 #include "spi.h"
 
+namespace mcp3208{
+
 /**
  * 各Channelを要素に持つenum class
  * MCP3208のチャンネルの指定に使用
  */
-enum class ADC_CHANNEL:uint8_t{
+enum class Channel:uint8_t{
 	CH_0,
 	CH_1,
 	CH_2,
@@ -22,7 +24,7 @@ enum class ADC_CHANNEL:uint8_t{
 /**
  * ADコンバータ MCP3208 から値を読み取るクラス
  */
-class MCP3208_reader{
+class MCP3208{
 private:
 	std::array<std::array<uint8_t,3>, 8> receive_data;						//!< ICから値を受け取る変数
 	std::array<std::array<uint8_t,3>, 8> transmit_data;						//!< ICに送る変数(値を取得したいChannelが格納)
@@ -30,12 +32,14 @@ private:
 	GPIO_TypeDef* ss_port;													//!< SSピンとして設定したGPIOのPort
 	const uint16_t ss_pin;													//!< SSピンとして設定したGPIOのPin
 public:
-	MCP3208_reader(SPI_HandleTypeDef& hspi,GPIO_TypeDef *ss_port, uint16_t ss_pin);
+	MCP3208(SPI_HandleTypeDef& hspi,GPIO_TypeDef *ss_port, uint16_t ss_pin);
 	void init();
-	void update(ADC_CHANNEL adc_channel, uint32_t timeout);
+	void update(Channel adc_channel, uint32_t timeout);
 	void update(uint32_t timeout);
-	uint16_t get(ADC_CHANNEL adc_channel);
+	uint16_t get(Channel adc_channel);
 	std::array<uint16_t,8> get();
 };
+
+} // namespace mcp3208
 
 #endif //INC_MCP3208T_READER_HPP_
